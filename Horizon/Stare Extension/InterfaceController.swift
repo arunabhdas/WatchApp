321731e5a10df06f12e877e1b453d25475bb52c9
Arunabh Das <arunabhdas@gmail.com>
2015-10-26 10:54:18 -0400

    Added Stare WatchKit app

---
+++ Horizon/Stare Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Stare Extension
+//
+//  Created by Arunabh Das on 2015-10-26.
+//  Copyright © 2015 Arunabh Das. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
