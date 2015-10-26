14693ce346e293ce21b3b5b732c54b6d7002dec3
Arunabh Das <arunabhdas@gmail.com>
2015-10-26 10:46:48 -0400

    Added Horizon

---
+++ Horizon/Horizon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Horizon
+//
+//  Created by Arunabh Das on 2015-10-26.
+//  Copyright © 2015 Arunabh Das. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Horizon/HorizonTests/HorizonTests.swift
@@ -0,0 +1,36 @@
+//
+//  HorizonTests.swift
+//  HorizonTests
+//
+//  Created by Arunabh Das on 2015-10-26.
+//  Copyright © 2015 Arunabh Das. All rights reserved.
+//
+
+import XCTest
+@testable import Horizon
+
+class HorizonTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
